ce74b43f5063c4b43c359099d1b682c275a3bf07
Zhu Jia Cheng <realalien@gmail.com>
2010-09-11 19:56:03 +0800

    daily update

---
+++ code_quality/object.rb
@@ -0,0 +1,52 @@
+
+
+require "test/unit"
+
+
+module Checkings
+  
+  def check_method_missing_usage
+    # static checking or dynamic checking? 
+    # e.g. has_defined_response_to_if_overriding_method_missing?()
+    
+  end
+  
+      
+  
+
+end
+
+class TestObject < Test::Unit::TestCase
+  
+  class A
+    def method_missing(name, *args)
+      
+    end
+    
+    def respond_to?
+      
+      
+    end
+    
+  end
+  
+  def test_check_method_missing_usage
+    
+  end
+end
+
+
+class A 
+  
+end
+
+puts "A.class\t=>\t\t#{A.class}"
+puts "A.superclass\t=>\t\t#{A.superclass}"
+puts "A.respond_to?(:method_missing)\t=>\t\t#{A.respond_to?(:method_missing)}"
+puts "A.respond_to?(:respond_to?)\t=>\t\t#{A.respond_to?(:respond_to?)}"
+# 
+puts "a = A.new " ; a = A.new
+puts "a.respond_to?(:method_missing)\t=>\t\t#{a.respond_to?(:method_missing)}"
+puts "a.respond_to?(:respond_to?)\t=>\t\t#{a.respond_to?(:respond_to?)}"
+
+
--- sandbox/scrapebook.rb
+++ sandbox/scrapebook.rb
@@ -19,17 +19,26 @@
 #  ---------------------------------------
 
 
-module Mod
-  def mod_method
-    puts "mod_method from Mod"    
-  end
-end  
+#module Mod
+#  def mod_method
+#    puts "mod_method from Mod"    
+#  end
+#end  
+#
+#def test_write_module_def_to_file(filename)
+#  include Mod
+#  
+#  #Q: how can I dynamically modify the include MO
+#  #IDEA: need extract methods to find candidates/potential useful methods from inputed keyword.
+#end
+#
+#test_write_module_def_to_file
 
-def test_write_module_def_to_file(filename)
-  include Mod
-  
-  #Q: how can I dynamically modify the include MO
-  #IDEA: need extract methods to find candidates/potential useful methods from inputed keyword.
-end
 
-test_write_module_def_to_file
+# STILL TESTING NEEDED
+
+#  ---------------------------------------
+
+
+# 
+

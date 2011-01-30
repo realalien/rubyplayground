09178520c353f614222715e6888f9a243077dbd2
Zhu Jia Cheng <realalien@gmail.com>
2011-01-30 17:10:10 +0800

    daily update

--- alice_builder/task.rb
+++ alice_builder/task.rb
@@ -1,12 +1,25 @@
 
 require "observer"
 
+# TODO, should have knowledge of color for legend
+class TaskStatus
+    OK          = 1
+    FAILED      = 2
+    TAINTED     = 3
+    RUNNING     = 4
+    UNAVAIL     = 5
+end
+
+
+# IDEA: if possible, the state machine can be used to allow actions take place during the status change, 
+#      it's very important to let known what to do if something goes unexpected.
 class Task
-    
+   
     include Observable 
     
     def initialize(name)
         @name = name
+        @status = nil
     end
     
     def plan(&how_to)
@@ -16,19 +29,47 @@ class Task
     end
     
     def run
+        # pre tasks
+        
+        # post tasks
+        
         if @how_to
-            @how_to.call
+            begin
+                @status = TaskStatus::RUNNING
+                @how_to.call
+            rescue             Exception => e 
+                puts "[ERROR] #{@name} FAILED. " # TODO: logging
+                puts e.message   
+                puts e.backtrace.inspect
+                puts "---------------------"
+                @status = TaskStatus::FAILED
+                return 
+            end
+            @status = TaskStatus::OK
         end
         # TODO: how to handle deferred/delayed/async result
         # if task ends successfuly        
-        
+    end
+    
+    # the 
+    def evaluate_result
+        return @status
+    end
+    
+    # debug use, not really for 
+    def set_status(status)
+        if status.is_a? TaskStatus
+            @status = status    
+        end
+        @status = TaskStatus::UNAVAIL
     end
 end
 
 
+
+# I think it should be part of task itself, or use pattern generator to create a list like object in order 
+# to give less coding.
 class TaskList
-    
-   
     attr_accessor :tasks
     
     def initialize
--- alice_builder/wxapp.rb
+++ alice_builder/wxapp.rb
@@ -3,7 +3,7 @@
 require "rubygems"
 require "wx"
 include Wx 
-
+require 'task.rb'
 
 # require "observable"
 
@@ -14,6 +14,9 @@ include Wx
 # * espcially serve the alice build process
 # * with factory method to create depend on a task from alice build
 # * with observer listening to the build status on one PC.
+
+# TODO: the legend of color should be closer to the UI part to be more apparent to the programmer, code's human aspect.
+
 class TaskPanel < Panel
 
 
@@ -28,23 +31,36 @@ class TaskPanel < Panel
         @task = task_to_listen   # TODO: decide if this is reference or shallow copy!
     end
         
-        
+    # SUG: do not add too many gadgets, it need explanations and prone to errors and doesn't help in recovering.    
     def install_ui
-        panel_sizer = BoxSizer.new(HORIZONTAL)
-        self.set_sizer(panel_sizer) 
+        @panel_sizer = BoxSizer.new(HORIZONTAL)
+        self.set_sizer(@panel_sizer) 
         
-        test_label = StaticText.new(self, -1, 'My Label Text', 
+        @test_label = StaticText.new(self, -1, 'My Label Text', 
                                     DEFAULT_POSITION, DEFAULT_SIZE, ALIGN_LEFT)
-        test_label.set_background_colour(Wx::GREEN)
+        @test_label.set_background_colour(Wx::GREEN)
 
         # TODO: should set the background of the panel to 
-        redo_button = Button.new(self, -1, 'Redo', DEFAULT_POSITION, DEFAULT_SIZE, ALIGN_RIGHT, DEFAULT_VALIDATOR, 'Redo')
-        recheck_button = Button.new(self, -1, 'Recheck', DEFAULT_POSITION, DEFAULT_SIZE, ALIGN_RIGHT, DEFAULT_VALIDATOR, 'Recheck')
-        panel_sizer.add(test_label, 50, Wx::ALL )
-        panel_sizer.add(redo_button, 25,  Wx::RIGHT)
-        panel_sizer.add(recheck_button, 25 , Wx::RIGHT)
+        @redo_button = Button.new(self, -1, 'Redo', DEFAULT_POSITION, DEFAULT_SIZE, ALIGN_RIGHT, DEFAULT_VALIDATOR, 'Redo')
+        @recheck_button = Button.new(self, -1, 'Recheck', DEFAULT_POSITION, DEFAULT_SIZE, ALIGN_RIGHT, DEFAULT_VALIDATOR, 'Recheck')
+        @panel_sizer.add(@test_label, 50, Wx::ALL )
+        @panel_sizer.add(@redo_button, 25,  Wx::RIGHT)
+        @panel_sizer.add(@recheck_button, 25 , Wx::RIGHT)
     end
     
+    # it is supposed to monitor task 24/7, but you know, things may change unexpected or beyond!
+    def monitor_task(task)
+        if task.is_a? Task  # do we have a more general design? 
+            @task = task
+            if @task.evalute_result == TaskStatus::OK
+                @test_label =  set_background_colour(Wx::GREEN)
+            else
+                @test_label =  set_background_colour(Wx::RED)
+            end
+        else
+            puts "UI now only listens to the status of task, it's not that general for anything else."
+        end
+    end
     
     
 end
@@ -63,17 +79,36 @@ class MinimalApp < App
         
         # TODO: question, why the two panel adding in, those panels are sequeezed into small area? 
  
-        t1 = Task.new "demo task"
-        t1.plan do 
-            5.times do 
-                
-            end
-        end
+         t1 = Task.new "demo task"
+         t2 = Task.new "demo task 2"
+         t2.set_status = TaskStatus::UNAVAIL
+#        t1.plan do 
+#            5.times do 
+#                puts "aaaaaa!"
+#            end
+#        end
+
+        vbox = Wx::BoxSizer.new( Wx::VERTICAL )
+ 
         
-        custom_panel = TaskPanel.new(f, -1)
+        custom_panel = TaskPanel.new(f, -1 )
         custom_panel.install_ui
-         
+        custom_panel.monitor_task(t1)
         
+        custom_panel2 = TaskPanel.new(f, -1 )
+        custom_panel2.install_ui
+        custom_panel.monitor_task(t2)
+        
+        vbox.add(custom_panel)
+        vbox.add(custom_panel2)
+        
+        # Set the window's sizer to the vbox
+        f.set_sizer( vbox )
+        # Make the application window the same size
+        # as the sizer
+        vbox.set_size_hints(f)
+
+
 #        status_panel = Panel.new(f)
 #        status_panel_sizer = BoxSizer.new(HORIZONTAL)
 #        status_panel.set_sizer(status_panel_sizer)

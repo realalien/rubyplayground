309d67414821b0be5b5b16e4932ac8e0ea1aa8de
Zhu Jia Cheng <realalien@gmail.com>
2012-04-27 20:11:06 +0800

    added digger for experimental

---
+++ hackingLBS/diggers.rb
@@ -0,0 +1,26 @@
+
+
+# because some tools are not smart enough to solve some problem, or requires too much coding for probing a feature, so interactivity is introduced, sth. like facemesh to collect info based on human intervention.
+class GameMaker
+
+    
+end
+
+
+class Digger
+
+    # NOTE: try to make it auto rather than manual search and 
+    #idea: use different strategies to select the best candidate.
+    def self.find_sina_weibo_account
+        
+    
+    end
+
+
+end
+
+
+
+class Analyzer
+
+end

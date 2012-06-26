a44c2a0a0b42f649630f5fa23905bd112e6bda45
realalien <realalien@gmail.com>
2012-06-26 18:08:56 +0800

    daily update

--- news.tracker/models.rb
+++ news.tracker/models.rb
@@ -4,5 +4,27 @@ require 'mongoid'
 class NewsPiece
 
     include Mongoid::Document 
-
+    include Mongoid::Timestamps::Created
+    
+    field :link, type: String   # 链接
+    field :content, type: String  # 文章或网页内容（尽量剔除不需要的内容）
+    
+    
 end
+
+
+
+
+module FinancialAccountability
+    
+end
+
+
+
+class AmbiguityNumber
+    
+    include Mongoid::Document
+    
+    
+end
+

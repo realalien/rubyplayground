05436bea7e618a6d4169915b98fb4e5c72942c54
Zhu Jia Cheng <realalien@gmail.com>
2012-10-24 01:00:10 +0800

    added some thoughts

---
+++ edu_catcher.rb
@@ -0,0 +1,103 @@
+#encoding:UTF-8
+
+
+# Purpose: find the organizations info from edu news and put it into an human life cycle management time-table, later can be extended to medical info/news analysis
+
+
+
+$AVG_HUMAN_LIFE_EXPAND = 110
+
+# Q: how to introduce locale (like apple's API) into the data retrieving?
+# A:
+# TODO: should read write to include new data!
+$EDU_LEVEL_TIMETABLE = {
+    "小学" => (7..11),
+    "初中" => (12..14),
+    "高中" => (15..17),
+    "本科" => (18..22),
+    "研究生" => (22..24),  # regular as planned, not accurate
+    "博士" => (22..26),
+    "博士后" => (26..28)
+}
+
+
+# TODO: should read write to include new data!
+$EDU_ORG = ["小学", "中学", "大学", "研究院"]
+
+
+
+# -------------------------------------------------------------
+# find the article with EDU_LEVEL info, also quote the sentence
+# -------------------------------------------------------------
+
+
+
+# --------------------- grab the content on target
+
+
+
+
+# ---------------------  parse sentence
+class String
+    # Q: how can I apply the functions to specific objects rather than alll the string objects
+    def has_edu_organization?
+        r = Regexp.new($EDU_ORG.join("|"))
+        if self.scan(r).size > 0
+            return true
+        else
+            return false
+        end
+    end
+    
+    def edu_organizations
+        r = Regexp.new($EDU_ORG.join("|"))
+        return self.scan(r)
+    end
+    
+end
+
+# ---------------------  find sentences with target info
+
+def has_edu_organization?(str)
+    mentions = []  # array of hash
+    
+    # TODO: how to look forward to include
+    ss = str.split(%r{；|。”|。})
+
+    ss.each do |s|
+        if s.has_edu_organization?
+            orgs = s.edu_organizations
+            mentions << { orgs => s }
+        end
+    end
+    
+    mentions.each do |item|
+        puts "#{item.keys[0]} -->   #{item[item.keys[0]]}"
+    end
+end
+
+
+
+
+
+
+
+
+
+# -------------------------------------------------------------
+#
+# -------------------------------------------------------------
+
+
+
+
+if __FILE__ == $0
+    s = "上海交通口腔医学院"
+    puts s.has_edu_organization?
+    
+    s = "谈闵星说，不同年级的候选人的问题也不同。“比如，刚加入少先队的三年级候选人的问题是‘在中队里，你曾经有过怎样的服务岗位，你在这个岗位上怎么做的’；上海交大口腔医学院前身是震旦大学医学院牙医系，设在广慈医院，是国内最早设立的口腔医学院校之一。”"
+    has_edu_organization?(s).each do |item|
+       puts "-->#{item}"
+    end
+    
+end
---
+++ news_simul.rb
@@ -0,0 +1,31 @@
+#encoding:UTF-8
+
+
+
+#Purpose:  find the similarity between the newsspaper to find the diff and detail
+
+
+def get_sections_xinmin_daily
+    
+    
+end
+
+
+
+
+
+
+if __FILE__ == $0
+
+
+# 1.0  find similar 'sections' between two newspapers, e.g. xm, whb, so that we can limit the text parsing context.
+# NOTE: the content should be grabbed in html format
+
+
+sections_from_xm = get_sections_xinmin_daily()
+
+    
+    
+# 1.1  find similar 
+
+end

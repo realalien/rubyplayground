185abb0d56a3af7029482f969026009231b51424
Zhu Jia Cheng <realalien@gmail.com>
2012-12-18 00:13:47 +0800

    added manual collected data

---
+++ sunlight.china/structures.rb
@@ -0,0 +1,44 @@
+#encoding:UTF-8
+
+ 
+# ---------------------------------------
+# Shanghai Workers Union (governing body)
+# ---------------------------------------
+# Note: inspired by http://goo.gl/FM9MT , with some manual Google searches
+
+org =  {
+    "name" =>  "上海市总工会", "website" => "www.shzgh.org"
+}
+
+
+org["sub-organizations"] = {
+    { "name" =>  "金山区总工会", "website" => "zgh.jinshan.gov.cn" },
+    { "name" =>  "闵行区总工会", "website" => "mhzgh.shmh.gov.cn" },
+    { "name" =>  "青浦区总工会", "website" => "tu.shqp.gov.cn" },
+    { "name" =>  "嘉定区总工会" },
+    { "name" =>  "松江区总工会" },
+    { "name" =>  "南汇区总工会" },
+    { "name" =>  "奉贤区总工会" },
+    
+    { "name" =>  "普陀区总工会", "website" => "www.ptzgh.org" },
+    { "name" =>  "虹口区总工会", },
+    { "name" =>  "闸北区总工会", },
+    { "name" =>  "宝山区总工会", },
+    { "name" =>  "杨浦区总工会", "website" => "www.shypzgh.org" },
+    
+    { "name" =>  "长宁区总工会", "website" => "www.cngh.org" },
+    { "name" =>  "徐汇区总工会", "website" => "zgh.xh.sh.cn" },
+    { "name" =>  "黄浦区总工会", "website" => "www.hpzgh.org" },
+    { "name" =>  "卢湾区总工会" },
+    { "name" =>  "静安区总工会" },
+    
+    { "name" =>  "浦东新区总工会", "website" => "gonghui.pudong.gov.cn" },
+}
+
+
+# ---------------------------------------
+# 
+# ---------------------------------------
+
+
+

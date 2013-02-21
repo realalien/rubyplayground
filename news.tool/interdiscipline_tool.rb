5badea614f1f45a546b46936ca8c0412d9747dc4
Zhu Jia Cheng <realalien@gmail.com>
2013-02-22 01:02:25 +0800

    save temp work, regex problem

--- news.tool/interdiscipline_tool.rb
+++ news.tool/interdiscipline_tool.rb
@@ -21,25 +21,27 @@ end
 
 class Conference < Event
   
-  $CONFERENCE_WORDS = ["conference","symposium","seminar","roundtable"]
+  $CONFERENCE_WORDS = ["conference","symposium","seminar","roundtable"].map{|a| [a, a.capitalize]}.flatten
   # find it  # SUG: try to refactor code to 'spot_from_text'
+  # TODO:  only English now!
   def self.spot_from_entry(entry, serious=true) #  , &blk block to allow to additional 
     #content = WebPageTool.retrieve_content(link).content
     
-    # TODO:  only English now!
+
     $MAX_LENGTH_FOR_NAME = 30
     text = entry.content
     # r = /(the.{,30}?conference)|\s(\w+Conf)|\s(\w+\-Con)|(the.{,30}?symposium)|(the.{,30}?seminar)|(the.{,30}?roundtable)/im # assuming there are no special chars
     
-    phrase_template = "(the\s[A-Z].{,30}?[[template]])" 
+    phrase_template = "(([A-Z][\\w\]*\\s)+[[template]])" 
     phrases = $CONFERENCE_WORDS.map{|a|phrase_template.clone.gsub("[[template]]", a) }
     str = phrases.join("|")
-    r = /#{str}|\s(\w+Conf)\b|\s(\w+\-Con)\b/im # assuming there are no special chars
+    r = /#{str}|\A(\w+Conf)\b|\A(\w+\-Con)\b/m # assuming there are no special chars
+    puts r
     result = text.scan(r).flatten.delete_if{|a|a.nil?}.map{|a|a.downcase}.uniq
-        
+    puts result
     
     if serious
-      r_may_need_human_check = /#{$CONFERENCE_WORDS.join('|')}/im
+      r_may_need_human_check = /#{$CONFERENCE_WORDS.join('|')}/m
       result_for_check = text.scan(r_may_need_human_check).flatten.delete_if{|a|a.nil?}.map{|a|a.downcase}.uniq
       if result.size != result_for_check.size
         puts "[WARNING] Conference alike words spotted, but not parsed out correctly, please check the text from link below"
@@ -61,7 +63,7 @@ class Conference < Event
     $MAX_LENGTH_FOR_NAME = 30
     #r = /(the.*?conference)|\s(\w+Conf)|\s(\w+\-Con)/
     
-    phrase_template = "(the\s[A-Z].*?[[template]])" 
+    phrase_template = "([the|a]\s[A-Z].*?[[template]])" 
     phrases = $CONFERENCE_WORDS.map{|a|phrase_template.clone.gsub("[[template]]", a) }
     str = phrases.join("|")
     r = /\s(\w+Conf)\B|\s(\w+\-Con)\B|#{str}/im # assuming there are no special chars
@@ -99,20 +101,25 @@ class Conference < Event
   def self.util_spot_from_rss(rss_link)
 
     feed = Feedzirra::Feed.fetch_and_parse(rss_link)
-    
-    feed.entries.first(5).each do |entry|
-     
-      #puts "#{entry.content.sanitze!}"
-      confs = self.spot_from_entry(entry)
-      #puts confs
-      if confs.size > 0
-        puts "~~   #{entry.title}   ~~"  #.sanitize
-        puts "Spot conference(s): #{confs.join(',')}" 
-        puts "----------------------"
+    if feed.is_a? Feedzirra::Parser::RSS
+      puts feed.entries.size
+      feed.entries.first(5).each_with_index do |entry, idx|
+        puts "#{idx}    --------------"
+        #puts "#{entry.content.sanitze!}"
+        confs = self.spot_from_entry(entry)
+        #puts confs
+        if confs.size > 0
+          puts "~~   #{entry.title}   ~~"  #.sanitize
+          puts "Spot conference(s): #{confs.join(',')}" 
+          puts "----------------------"
+        end
       end
+    else
+      puts "Feed got appears to be #{feed}"
+      puts "Can't get the feeds from #{rss_link}!"
+      []
     end
   end
-  
 end
 
 
@@ -120,7 +127,7 @@ end
 if __FILE__ == $0
   # Idea: given a link marked/tagged with 'journalism'(discipline) or 'rss'(format), it should be able to regulary check for new information to process once initiated! 
  
-  nieman_rss = "http://feeds.feedburner.com/NiemanJournalismLab"
+  nieman_rss = "http://www.niemanlab.org/feed/" # "http://feeds.feedburner.com/NiemanJournalismLab"; #  http://www.google.com/reader/view/feed/http%3A%2F%2Fwww.niemanlab.org%2Ffeed%2F
   confs = Conference.util_spot_from_rss(nieman_rss)
   
 end

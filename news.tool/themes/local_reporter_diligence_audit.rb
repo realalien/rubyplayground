027bf0f350042f577e0177ec51aea85e025e2c91
realalien <realalien@gmail.com>
2013-05-28 18:07:06 +0800

    daily update

---
+++ news.tool/themes/local_reporter_diligence_audit.rb
@@ -0,0 +1,9 @@
+#encoding:UTF-8
+
+# Purpose: some reporters did the news on financial data like FDI, find other articles of that 
+#  reporter and do a monthly/yearly check up for consistency and validness.
+# e.g. http://xmwb.xinmin.cn/html/2012-05/27/content_2_7.htm
+# WISH: use local search engine to find related news articles!
+
+
+

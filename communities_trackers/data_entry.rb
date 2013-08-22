8c70c93b34ac83c5d3daaab1928413e73abfdebb
realalien <realalien@gmail.com>
2013-08-22 17:30:02 +0800

    daily update

---
+++ communities_trackers/data_entry.rb
@@ -0,0 +1,12 @@
+
+# used for data entry in his own way!
+require File.join( File.dirname(__FILE__) , "../../hackingLBS/ddmap_resources.rb" )
+
+
+# NOTE: places from ddmap will be saved as files first, see why it's necessary to insert into db!
+class DingDingMapDataEntry
+    
+    
+    
+    
+end

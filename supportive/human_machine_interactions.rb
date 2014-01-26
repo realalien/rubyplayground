e126e9365e904a1e3955e1855183fa50cd72a3d6
Zhu Jia Cheng <realalien@gmail.com>
2014-01-26 21:34:14 +0800

    merge

---
+++ supportive/human_machine_interactions.rb
@@ -0,0 +1,89 @@
+#encoding: UTF-8
+
+
+# Coded Notes on 'The design of future things', Don Norman
+# Detailing the following questions
+
+
+Context "what's the obvious software feedback if something goes wrong or it can't handle? Some physical sympton like 'straightened, tense, beeping or sound feedback? " do
+	listing do 
+			[""] //TODO
+	end
+
+end
+
+
+
+Context "software supportive functionalities and how it is implemented" do
+	eg "experimenting with partial correction"
+	eg "signaling users"
+	eg "does give users any say in ineraction"
+	eg "let user know which system can be trusted via demos"
+  eg "how to handle if things does not go as planned."
+end
+
+
+Context "intelligence" do
+	listing do 
+		["initiatives", "intelligence", "emotion", "personalities"]
+  end
+  eg "Collaboration via synchronizing user's acitivities, as well as explaining and giving reasons. Through experience and understanding"
+
+  eg "Socrates: a technology that gives no opportunities for discussion, explanation, or debate is poor tech, the PROCESS of making a decision is often more important than the decision itself."
+
+  eg "know why system thinks in one way"
+
+	listing do 
+		["safety", "convenience", "accuracy"]
+	end
+
+end
+
+
+Context "interaction consideration between machine and human" do
+	eg "change the way we interact with our machines to take better advantage of their strengths and virtues, while at the same time eleminating their annoying and sometimes dangerous actions"
+
+  eg "whenever a task is only partially automated, it is essential that each party, human and machine, know what the other is doing and what is intended."
+
+	eg "successful dialogue requires shared knowledge and experiences. It requires appreciation of the env and context, of the history leading up to the moment, and of the many differing goals and motives of the people involved."
+
+	eg "the propery way to provide for smooth interaction between people and intelligent device is to enhance the coordination and cooperation of both parties, people and machines"
+
+	eg "must design our tech for the way people actually behave, not the way we would like them to behave"
+
+	eg "determine the appropriate response to something unexpected? When this happens to a person, we can expect creative, imagninative problem solving"
+
+	eg "understand the goals and motives of the people with whom they must interact, the special circustances that invariably surround any set of activities."
+
+  eg "social skills, creativity, imagination, empathy"
+
+	eg "it doesn't really matter whether the machine or the person is correct: it is the mismatch that matters, for this is what gives rise to aggravation, frustration, and in some cases, damage or injury"
+
+	eg "let interested parties to know why the machine is doing this "
+  
+ 
+
+ 
+
+
+end
+
+
+Context "software features to be implemented" do
+  eg "ask for details or seek some modification, better conversation to the lists for users who are not so knowledgeable!" 
+end
+
+Context "in the near future" do 
+	eg "monitor user eating, reading, music and tel preferences, watch where you drive, alerting insurance company, rental agency or even police, system makes gross assumptions about your intentions from a limited sample of your behaviors"
+
+
+
+
+end
+
+
+
+xf Notes:
+*  actual: "我1885点怎么样" （datetime, 01月26日，5:00）
+   expect:  "（俏江南）1885店怎么样"
+
